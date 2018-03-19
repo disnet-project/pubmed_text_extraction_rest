28128944d2a84ba941f205dd7f3dfe72de66e11c
Gerardo Lagunes García <gerardolagar@ctb.upm.es>
2018-03-19 10:38:05 +0100

    Aumento del tiempo de espera para obtener documento Web con Jsoup.

--- src/main/java/es/upm/disnet/pubmed/parser/PubMedWebArticleParser.java
+++ src/main/java/es/upm/disnet/pubmed/parser/PubMedWebArticleParser.java
@@ -32,7 +32,7 @@ public class PubMedWebArticleParser {
             Document doc = Jsoup.connect(url)
                     .userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6")
                     .referrer("http://www.google.com")
-                    .timeout(60*1000)
+                    .timeout(300*1000)
                     .get();
 
             Element abstractElement = doc.getElementsByTag("abstracttext").first();
